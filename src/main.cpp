e40af0a923654dc2d6608d07adad80e99c9c6c23
Amit Vijairania <amit.vijairania@gmail.com>
2019-10-11 16:27:44 -0700

    add spline

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ using std::vector;
 int main() {
   uWS::Hub h;
 
-  // Load up map values for waypoint's x,y,s and d normalized normal vectors
+  // Load up map values for waypoint's x, y, s and d normalized normal vectors
   vector<double> map_waypoints_x;
   vector<double> map_waypoints_y;
   vector<double> map_waypoints_s;
@@ -33,6 +33,7 @@ int main() {
   std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
   string line;
+
   while (getline(in_map_, line)) {
     std::istringstream iss(line);
     double x;
@@ -52,13 +53,15 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  int lane = 1;    // 0 is left lane, 1 is middle lane (car starts here), and 2 is right lane.
+
+  h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -92,13 +95,110 @@ int main() {
 
           json msgJson;
 
+
+          ///// NOTES_AV /////
+
+          double goal_v = 49.5;    // Goal velocity of 49.5 MPH.
+
+          int previous_path_size = previous_path_x.size();    // Previous path size in number of points (50).
+
+          //if (previous_path_size > 0) {
+          //  car_s = end_path_s;
+          //}
+
+          bool tailgating = false;    // Driving too closely behind another vehicle.
+
+          // Sparse waypoints to be interpolated by Spline.
+          vector<double> spline_waypoints_x;
+          vector<double> spline_waypoints_y;
+
+          double curr_x = car_x;
+          double curr_y = car_y;
+          double angle = deg2rad(car_yaw);
+
+          if (previous_path_size < 2) {
+            spline_waypoints_x.push_back(car_x - cos(car_yaw));
+            spline_waypoints_x.push_back(car_x);
+
+            spline_waypoints_y.push_back(car_y - sin(car_yaw));
+            spline_waypoints_y.push_back(car_y);
+          } else {
+            spline_waypoints_x.push_back(previous_path_x[previous_path_size - 2]);
+            spline_waypoints_x.push_back(previous_path_x[previous_path_size - 1]);
+
+            spline_waypoints_y.push_back(previous_path_y[previous_path_size - 2]);
+            spline_waypoints_y.push_back(previous_path_y[previous_path_size - 1]);
+          }
+
+          // Location of car in 30, 60, and 90 meters.
+          vector<double> spline_wp30 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> spline_wp60 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> spline_wp90 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          spline_waypoints_x.push_back(spline_wp30[0]);
+          spline_waypoints_x.push_back(spline_wp60[0]);
+          spline_waypoints_x.push_back(spline_wp90[0]);
+
+          spline_waypoints_y.push_back(spline_wp30[1]);
+          spline_waypoints_y.push_back(spline_wp60[1]);
+          spline_waypoints_y.push_back(spline_wp90[1]);
+
+          for (int i = 0; i < spline_waypoints_x.size(); i++) {
+            // Shift car reference angle to 0 degree.
+            double shift_x = spline_waypoints_x[i] - curr_x;
+            double shift_y = spline_waypoints_y[i] - curr_y;
+
+            spline_waypoints_x[i] = shift_x * cos(0 - angle) - shift_y * sin(0 - angle);
+            spline_waypoints_y[i] = shift_x * sin(0 - angle) + shift_y * cos(0 - angle);
+          }
+
+          // Create a spline
+          tk::spline s;
+
+          // Add x, y points on the spline
+          s.set_points(spline_waypoints_x, spline_waypoints_y);
+
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          for (int i = 0; i < previous_path_size; i++) {
+            next_x_vals.push_back(previous_path_x[i]);    // AV: Shouldn't we get x, y from end of previous path?
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // Calculate spline points to travel
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x * target_x + target_y * target_y);
+
+          double x_addon = 0.0;
+          for (int i = 1; i <= 50 - previous_path_size; i++) {
+
+            if(goal_v > car_speed) {
+              car_speed += .224;
+            } else if(goal_v < car_speed) {
+              car_speed -= .224;
+            }
+
+            double N = target_dist / (0.02 * car_speed / 2.24);    // MPH to m/s
+            double x_point = x_addon + target_x / N;
+            double y_point = s(x_point);
+            x_addon = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = x_ref * cos(angle) - y_ref * sin(angle);
+            y_point = x_ref * sin(angle) + y_ref * cos(angle);
+
+            x_point += x_ref;
+            y_point += y_ref;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+
+          ///// END_AV /////
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
